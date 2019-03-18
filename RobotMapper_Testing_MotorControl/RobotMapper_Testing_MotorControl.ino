3361d17a3dc8724b174edb5ce40c611ca5ea8cbf
sciencemike <m.reid@photonsystems.com>
2019-03-18 00:58:11 -0700

    Update RobotMapper_Testing_MotorControl.ino
    Functionality for the state machine is now added in a rudimentary way.
    Next is the 10DOF sensor.

--- RobotMapper_Testing_MotorControl/RobotMapper_Testing_MotorControl.ino
+++ RobotMapper_Testing_MotorControl/RobotMapper_Testing_MotorControl.ino
@@ -11,6 +11,7 @@
 #include <Servo.h>
 
 // -----------------Globals---------------------------
+
 int motorPauseDelay = 50;
 Servo myservo;
 LIDARLite lidarLite;
@@ -20,6 +21,7 @@ int cal_cnt = 0;
 #define motor2Pin1 7
 #define motor2Pin2 9
 int pos = 0;
+// -----------------END Globals---------------------------
 
 void setup() {
   pinMode(motor1Pin1, OUTPUT);
@@ -33,8 +35,45 @@ void setup() {
 }
 
 void loop() {
+    float opCode = 0;
+    int firstDigitOpcode = 0;
+    int secondDigitOpcode = 0;
+    int fifthDigitOpcode = 0;
+    int seventhDigitOpcode = 0;
+    if (Serial.available()) {
+        opCode = Serial.parseInt();
+        //opCode = Serial.read();
+        if (opCode == 0){
+            return;
+        }
+        firstDigitOpcode = opCode/100000;
+        secondDigitOpcode = (opCode -firstDigitOpcode *100000)/1000;
+        fifthDigitOpcode = (opCode - (firstDigitOpcode*100000 +secondDigitOpcode*1000))/10;
+        seventhDigitOpcode = opCode - (firstDigitOpcode*100000 +secondDigitOpcode*1000 +fifthDigitOpcode*10); 
+        
+        Serial.println (opCode); 
+        Serial.println (firstDigitOpcode); 
+        Serial.println (secondDigitOpcode); 
+        Serial.println (fifthDigitOpcode); 
+        Serial.println (seventhDigitOpcode);
+        stateMachine(firstDigitOpcode, secondDigitOpcode,fifthDigitOpcode);
+      }
+}
+void moveRight(int timeDelay){
+  digitalWrite(motor1Pin1, HIGH); 
+  digitalWrite(motor1Pin2, LOW);   
+  delay(timeDelay);              
+  digitalWrite(motor1Pin1, LOW);  
+  digitalWrite(motor1Pin2, LOW);
+}void moveLeft(int timeDelay){
+  digitalWrite(motor1Pin1, LOW); 
+  digitalWrite(motor1Pin2, HIGH);   
+  delay(timeDelay);              
+  digitalWrite(motor1Pin1, LOW);  
+  digitalWrite(motor1Pin2, LOW);
+}
+void continuousScanner (){
   int dist;
-  
   digitalWrite(motor1Pin1, HIGH); 
   digitalWrite(motor1Pin2, LOW);   
   delay(2);              
@@ -42,20 +81,72 @@ void loop() {
   digitalWrite(motor1Pin2, LOW);
   
  for (pos = 50; pos <= 100; pos += 10) { 
-    
     myservo.write(pos);              
     delay(motorPauseDelay);
-    dist = lidarLite.distance(false); 
+    dist = getLidarDistance(); 
     Serial.print(dist);     
     Serial.println(" cm");
-    
   }
   for (pos = 100; pos >= 50; pos -= 10) { 
-    myservo.write(pos);             
-    dist = lidarLite.distance(false);      
+    myservo.write(pos);   
+    delay(motorPauseDelay);            
+    dist = getLidarDistance();      
     Serial.print(dist);
     Serial.println(" cm");
-    delay(motorPauseDelay);  
-
   }
+}
+
+float getLidarDistance ()
+{
+  int val1 = lidarLite.distance();
+  int val2 = lidarLite.distance();
+  int val3 = lidarLite.distance();
+  int val4 = lidarLite.distance();
+  int val5 = lidarLite.distance();
+  int val6 = lidarLite.distance();
+  int val7 = lidarLite.distance();
+  int val8 = lidarLite.distance();  
+  int val9 = lidarLite.distance();
+  int val10 = lidarLite.distance();
+  int val11 = lidarLite.distance();
+  int val12 = lidarLite.distance();
+  float aveVal = (val1+val2+val3+val12+val4+val5+val6+val7+val8+val9+val10+val11)/12.0;
+  return (aveVal);
+}
+
+void stateMachine( int Code, int secondDigit, int thirdDigit){ 
+    switch (Code) {
+        case 0:
+            //Serial.write(Code);
+            break;
+          
+        case 1:    // Get Distance
+            Serial.println(Code);
+            Serial.println(getLidarDistance ());
+            break;
+          
+        case 2:    // Move Right
+            //Serial.println(Code,secondDigit);
+            moveRight(secondDigit);
+            myservo.write(thirdDigit);
+            break;
+          
+        case 3:  // Move Left
+            //Serial.println(Code,secondDigit);
+            moveLeft(secondDigit);
+            myservo.write(thirdDigit);
+            break;
+                      
+        case 4:  // Move UP
+            Serial.println(Code,thirdDigit);
+            break; 
+                      
+        case 5:  // Move Down
+            Serial.println(Code,thirdDigit);
+            break; 
+                  
+        default:
+            Serial.println("default");
+            break;
+    }
 }
